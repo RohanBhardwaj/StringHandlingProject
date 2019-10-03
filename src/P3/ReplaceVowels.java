187bfd88b3843a8187b80ef56accdb03471d5944
RohanBhardwaj <rohan.bhardwaj_cs18@gla.ac.in>
2019-10-03 22:38:34 +0530

    final Commit

--- src/P3/ReplaceVowels.java
+++ src/P3/ReplaceVowels.java
@@ -1,4 +1,17 @@
 package P3;
 
+import java.util.Scanner;
+
 public class ReplaceVowels {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String s1 = scanner.next();
+        //replace replaces all the occurrence of oldChar with newChar
+        s1 = s1.replace('a', '*');
+        s1 = s1.replace('e', '*');
+        s1 = s1.replace('i', '*');
+        s1 = s1.replace('o', '*');
+        s1 = s1.replace('u', '*');
+        System.out.println(s1);
+    }
 }
