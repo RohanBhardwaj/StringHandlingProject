6b393f603d4734907e6c1e46e1ae4a4e7bb53ffd
RohanBhardwaj <rohan.bhardwaj_cs18@gla.ac.in>
2019-10-03 22:23:40 +0530

    final commit

--- src/P2/pro2.java
+++ src/P2/pro2.java
@@ -1,6 +1,13 @@
 package P2;
 
-public class pro2 {
+import java.util.Scanner;
 
+public class pro2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String inputString = scanner.nextLine();
+        int index = Integer.parseInt(scanner.next().trim());
+        System.out.print("Substring: " + inputString.substring(index));
+    }
 }
 
