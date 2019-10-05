b393415c06f077d2ad4dde2a6eea4d3c9d806951
RohanBhardwaj <rohan.bhardwaj_cs18@gla.ac.in>
2019-10-05 17:08:45 +0530

    final commit

--- src/P11/Indexofall.java
+++ src/P11/Indexofall.java
@@ -1,4 +1,20 @@
 package P11;
 
+import java.util.Scanner;
+
 public class Indexofall {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the string:");
+        String str = scanner.next();
+        System.out.println("Enter the character");
+        char ch = scanner.next().charAt(0);
+        char[] chars = str.toCharArray();
+        for (int i = 0; i < str.length(); i++) {
+            if (chars[i] == ch) {
+                System.out.println(i);
+            }
+        }
+
+    }
 }
