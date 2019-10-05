53f724628f08a06ba6d24094697fb551a798d0a7
RohanBhardwaj <rohan.bhardwaj_cs18@gla.ac.in>
2019-10-05 12:14:47 +0530

    final commit

--- src/P8/UpperWeight.java
+++ src/P8/UpperWeight.java
@@ -1,4 +1,20 @@
 package P8;
 
+import java.util.Scanner;
+
 public class UpperWeight {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the String: ");
+        String str = scanner.next();
+        char[] s2 = str.toCharArray();
+        int Upperweight = 0;
+        for (int i = 0; i < str.length(); i++) {
+            if (s2[i] >= 65 && s2[i] <= 90) {
+                Upperweight += s2[i];
+            }
+        }
+        System.out.println(Upperweight);
+
+    }
 }
