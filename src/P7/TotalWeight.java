3d78794c7623cb6b24a089024ad9181e2e6d15bf
RohanBhardwaj <rohan.bhardwaj_cs18@gla.ac.in>
2019-10-05 11:52:22 +0530

    final commit

--- src/P7/TotalWeight.java
+++ src/P7/TotalWeight.java
@@ -1,4 +1,18 @@
 package P7;
 
+import java.util.Scanner;
+
 public class TotalWeight {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the String: ");
+        String str = scanner.nextLine();
+        char[] s2 = str.toCharArray();
+        int Totalweight = 0;
+        for (int i = 0; i < str.length(); i++) {
+            Totalweight += (int) s2[i];
+        }
+        System.out.println("Total Weight is: " + Totalweight);
+
+    }
 }
