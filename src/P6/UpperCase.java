029b999caab9f0be1dc855c3ecd43cc10af5fe9a
RohanBhardwaj <rohan.bhardwaj_cs18@gla.ac.in>
2019-10-05 11:43:41 +0530

    final commit

--- src/P6/UpperCase.java
+++ src/P6/UpperCase.java
@@ -1,4 +1,13 @@
 package P6;
 
+import java.util.Scanner;
+
 public class UpperCase {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the string");
+        String s1 = scanner.nextLine();
+        String s2 = s1.toUpperCase();
+        System.out.println("Upper case string: " + s2);
+    }
 }
