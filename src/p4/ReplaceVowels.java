6adaf7b7bf7b684133ddb95cfbf63273639a57fe
RohanBhardwaj <rohan.bhardwaj_cs18@gla.ac.in>
2019-10-05 11:05:39 +0530

    added replace conditions

--- src/p4/ReplaceVowels.java
+++ src/p4/ReplaceVowels.java
@@ -1,4 +1,27 @@
 package p4;
 
+import java.util.Scanner;
+
 public class ReplaceVowels {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String s1 = scanner.nextLine();
+        char[] s2 = s1.toCharArray();
+        int c = 0;
+        for (int i = 0; i < s1.length(); i++) {
+            if (s1.charAt(i) == 'a' || s1.charAt(i) == 'e' || s1.charAt(i) == 'i' || s1.charAt(i) == 'o' || s1.charAt(i) == 'u') {
+                if (c == 0) {
+                    s2[i] = '*';
+                }
+                if (c == 1) {
+                    s2[i] = '^';
+                }
+                if (c == 2) {
+                    s2[i] = '!';
+                }
+                c++;
+            }
+        }
+        System.out.println(s2);
+    }
 }
