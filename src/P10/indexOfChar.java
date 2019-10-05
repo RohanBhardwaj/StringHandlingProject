3d0cb5feecacef06bdcc6a1854d7bc20885c8a07
RohanBhardwaj <rohan.bhardwaj_cs18@gla.ac.in>
2019-10-05 17:01:28 +0530

    final commit

--- src/P10/indexOfChar.java
+++ src/P10/indexOfChar.java
@@ -1,4 +1,15 @@
 package P10;
 
+import java.util.Scanner;
+
 public class indexOfChar {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the string:");
+        String str = scanner.next();
+        System.out.println("Enter the character");
+        char ch = scanner.next().charAt(0);
+        int n = str.indexOf(ch);
+        System.out.println(n);
+    }
 }
